81e43407d068535cf52829074f24f507d519777b
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-04-23 19:52:04 +0600

    added not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col justify-center items-center h-[40vh]">
+      <h1 className="text-6xl lg:text-8xl font-bold text-red-400">404</h1>
+      <h5 className="font-bold text-3xl lg:text-5xl text-red-700">
+        Page not found
+      </h5>
+    </div>
+  );
+}
