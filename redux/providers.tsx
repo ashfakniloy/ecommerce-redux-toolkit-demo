d81cbf1c0a8beefe93dd8e08d78ad27a153450d4
Ashfak Niloy <ashfakniloy56@gmail.com>
2024-04-24 16:07:08 +0600

    added redux persist

--- redux/providers.tsx
+++ redux/providers.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistConfig, persistStore } from "redux-persist";
+import { RootState } from "@reduxjs/toolkit/query";
+// import { localStorage } from "redux-persist/es/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,15 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./features/cart/cart-slice";
 import { productsApi } from "./api/products-api";
 
+const persistConfig = {
+  key: "cart",
+  storage,
+  blacklist: ["showCart"],
+};
+
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
+
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedCartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
